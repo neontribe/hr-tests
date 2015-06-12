8b1d80068dbfdaa8ec1377cae90a0ab617331884
Oliver Barnwell <blastdoors666@gmail.com>
2015-06-12 09:27:02 +0100

    Add shortlist checking

--- test_cottage_page.js
+++ test_cottage_page.js
@@ -77,18 +77,13 @@ casper.start(casper.cli.get("target"), function() {}).run(function() {
 						return parseInt(document.getElementsByClassName("fulldescription")[0].style.height.split("px")[0], 10);
 					});
 					test.assertTrue(new_height > tmp_height, "Check that read more button expands the info section.");	
-					
 					casper.clickLabel("Read more", "a");
 				}).then(function() {
 					test.comment("Collapsing read more.")
-					
-					
-				new_height = casper.evaluate(function() {
-							return parseInt(document.getElementsByClassName("fulldescription")[0].style.height.split("px")[0], 10);
-				});
-						
-				test.assertTrue(tmp_height < new_height  , "Check that the read more button 'un-expands' the description");
-					
+					new_height = casper.evaluate(function() {
+								return parseInt(document.getElementsByClassName("fulldescription")[0].style.height.split("px")[0], 10);
+					});
+					test.assertTrue(tmp_height < new_height  , "Check that the read more button 'un-expands' the description");
 				});
 			});
 		}).then(function() {
@@ -102,7 +97,6 @@ casper.start(casper.cli.get("target"), function() {}).run(function() {
 					});
 					test.assertTrue(state.indexOf("showing") > -1, "Check that the reviews panel has class 'showing'.");
 					test.assertVisible("#reviews", "Check that the reviews panel is visible.");
-
 					test.comment("Click Prices and availability tab");
 					casper.click("a[href='#prices-and-availability']");
 				}).then(function() {
@@ -127,8 +121,33 @@ casper.start(casper.cli.get("target"), function() {}).run(function() {
 					});
 					test.assertTrue(state.indexOf("showing") > -1, "Check that the property-overview tab has class 'showing'.");
 					test.assertVisible("#property-overview", "Check that the property-overview panel is visible.");
-					
 				});
+		}).then(function() {
+			//Check shortlist functionality.
+			test.comment("Clicking add to shortlist button.");
+			casper.click("a.add-to-shortlist");
+			casper.waitForSelectorTextChange("a.add-to-shortlist", function(e) {
+				var btn = casper.evaluate(function() {
+					return document.getElementsByClassName("add-to-shortlist")[0].innerText.replace(" ", "");
+				});
+				test.assertTrue(btn == "Remove", "Check shortlist button text has changed to 'remove'.")
+			});
+		}).then(function() {
+			test.comment("Ascertaining whether property is on shortlist page.");
+			casper.clickLabel("Your shortlist");
+			casper.then(function() {
+				test.assertTrue(this.getCurrentUrl() == url + "shortlist/", "Check that shortlist page has loaded correctly.");
+				test.comment("Grabbing properties on page.");
+				var properties_returned_names = casper.evaluate(function() {
+					var properties_in_document = document.querySelectorAll(".rich-listing");
+					return Array.prototype.map.call(properties_in_document, function(e) {
+						return e.getElementsByClassName("title-and-review")[0].getElementsByTagName("h3")[0].getElementsByTagName("a")[0].innerText;
+					});
+				});
+
+				test.comment("Checking if there is a matching property.");
+				test.assert(properties_returned_names.indexOf(tmp_cottage_to_check) > -1, "Check that shortlist properties includes test property.");
+			});
 		}).run(function() {
 			test.done();
 		}).viewport(1920, 1080);
